fb8606aac0bfd4176cedc5369321ac72f4ad211d
unknown <rahult016@gmail.com>
2024-01-16 12:56:45 +0530

    changes

--- excel_api/utils.py
+++ excel_api/utils.py
@@ -1,7 +1,7 @@
 from django.db import models, connection
 
 def get_field_type():
-    return models.CharField(max_length=255)
+    return models.CharField(max_length=255,null=True,blank=TR)
 
 def create_dynamic_table(model_name, data):
     model_name = model_name.replace(" ", "_")
