f7e16467ffa09f939adea9c43136b526856bc0b1
unknown <rahult016@gmail.com>
2024-01-16 12:10:16 +0530

    add requiremesnts.txt

--- excel_api/utils.py
+++ excel_api/utils.py
@@ -1,39 +1,28 @@
-from django.db import models, connections
+from django.db import models, connection
 
-connection = connections["default"]
-
-def get_field_type(column_value):
-    if isinstance(column_value, int):
-        return models.IntegerField()
-    elif isinstance(column_value, float):
-        return models.FloatField()
-    elif isinstance(column_value, bool):
-        return models.BooleanField()
-    elif isinstance(column_value, str):
-        return models.CharField(max_length=255)  # Specify max_length for CharField
-    else:
-        return models.CharField(max_length=255)  # Specify max_length for CharField
+def get_field_type():
+    return models.CharField(max_length=255)
 
 def create_dynamic_table(model_name, data):
+    model_name = model_name.replace(" ", "_")
+    app_label = 'excel_api'
 
-    class BaseModel(models.Model):
-        pass
-
+    # Create dynamic model
+    fields = {}
     for column_name in data.columns:
         field_name = column_name.replace(" ", "_")
-        field_type = get_field_type(data[column_name].iloc[0])  # Use the first row to determine the field type
-        setattr(BaseModel, field_name, field_type)
+        field_type = get_field_type()  # Use the get_field_type function without arguments
+        fields[field_name] = field_type
 
-    model_name = model_name.replace(" ", "_")
-    dynamic_model = type(model_name, (BaseModel,), {"__module__": __name__})
-    dynamic_model._meta.app_label = 'excel_api'
-    dynamic_model._meta.db_table = f'dynamic_table_{model_name}'
-    dynamic_model._meta.managed = False
-    dynamic_model.save()
+    # Create Meta class for the dynamic model
+    meta_class = type('Meta', (), {'app_label': app_label})
 
-    for _, row in data.iterrows():
-        instance = dynamic_model()
-        for column_name in data.columns:
-            field_name = column_name.replace(" ", "_")
-            setattr(instance, field_name, row[column_name])
-        instance.save()
+    # Create dynamic model class
+    dynamic_model = type(model_name, (models.Model,), {'__module__': __name__, 'Meta': meta_class, **fields})
+
+    # Create the table in the database
+    with connection.schema_editor() as schema_editor:
+        schema_editor.create_model(dynamic_model)
+
+    return dynamic_model
+
